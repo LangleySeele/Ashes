f8324c931a5269c4caa64682a2d8c5ed0e91d0bb
LangleySeele <57202687+LangleySeele@users.noreply.github.com>
2019-11-24 04:59:19 -0600

    Update welcome.blade.php
    nuevo welcome con los personajes, dialogos e historia

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,48 @@
             <div class="header-body text-center mb-7">
                 <div class="row justify-content-center">
                     <div class="col-lg-5 col-md-6">
-                        <h1 class="text-white">{{ __('ASHes') }}</h1>
+                        <!--<h1 class="text-white">{{ __('ASHes') }}</h1>
+                        <a href='/characters' class="nav-link text-primary">
+                        <i><img style="height: 100px; width: 100px; background-color: #EFEFEF; margin: 20px;
+			" class="card-img-top rounded-circle mx-auto d-block" src="images/Actor1.png" alt=""></i> {{ __('Characters') }}
+                         </a>-->
+                        <div class="col-sm" aling="left">
+			            <div class="card text-center " style="width: 18rem; margin-top: 70px;">
+			            <img style="height: 100px; width: 100px; background-color: #EFEFEF; margin: 20px;
+			                " class="card-img-top rounded-circle mx-auto d-block" src="images/Actor1.png" alt="">
+		                <div class="card-body">
+			            <h5 class="Card-title">Personajes</h5>
+			            <h5 class="Card-text">Aqui podras ver a los personajes del videojuego.</h5>
+			            <a href="/characters" class="btn btn-primary">Ver mas...</a>
+		                </div>
+	                    </div>
+
+                        <div class="col-sm">
+			            <div class="card text-center " style="width: 18rem; margin-top: 70px;">
+			            <img style="height: 100px; width: 100px; background-color: #EFEFEF; margin: 20px;
+			                " class="card-img-top rounded-circle mx-auto d-block" src="images/0357caa9ff68093.jpg" alt="">
+		                <div class="card-body">
+			            <h5 class="Card-title">Historia</h5>
+			            <h5 class="Card-text">Aqui podras ver la historia que esta escrita hasta ahora.</h5>
+			            <a href="/historia" class="btn btn-primary">Ver mas...</a>
+		                </div>
+	                    </div>
+
+                        <div class="col-sm">
+			            <div class="card text-center " style="width: 18rem; margin-top: 70px;">
+			            <img style="height: 100px; width: 100px; background-color: #EFEFEF; margin: 20px;
+			                " class="card-img-top rounded-circle mx-auto d-block" src="images/003.png" alt="">
+		                <div class="card-body">
+			            <h5 class="Card-title">Dialogos</h5>
+			            <h5 class="Card-text">Aqui podras ver los dialogos de cada personaje por escena.</h5>
+			            <a href="/dialogs" class="btn btn-primary">Ver mas...</a>
+		                </div>
+	                    </div>
+
+                        <!--<a href='/historia' class="nav-link text-primary">
+                        <i><img style="height: 100px; width: 100px; background-color: #EFEFEF; margin: 20px;
+			" class="card-img-top rounded-circle mx-auto d-block" src="images/0357caa9ff68093.jpg" alt=""></i> {{ __('Historia') }}
+                        </a>-->
                         <p class="text-lead text-light">
                             {{ __('Existen tantas verdades como personas.') }}
                         </p>
