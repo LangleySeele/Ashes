23c6c9953cc811545e4a39ef461b3fedc4644a09
LangleySeele <57202687+LangleySeele@users.noreply.github.com>
2019-12-01 01:32:01 -0600

    Update show.blade.php
    se modifica la vista de los personajes

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,10 +1,33 @@
 @extends('layouts.app')
 @section('title','characters')
 @section('content')
-		<img style="height: 100px; width: 100px; background-color: #EFEFEF; margin: 20px;" class="card-img-top rounded-circle mx-auto d-block" src="../images/{{$character->avatar}}" alt="">
-		<h5 class="text-center">{{$character->name}}</h5>
-		<div class="text-center">
-			<p>{{$character->description}}</p>
+		
+			<table>
+		<div>
+			<tr>
+			<td><img style="height: 250px; width: 250px; background-color: #EFEFEF; margin: 20px;" 
+			class="center" src="../images/{{$character->avatar}}" alt=""> 
+			</td>
+			<td>
+			<h3 class="text-center">Nombre: {{$character->name}}</h3>
+			<h3 class="text-center">Apellido: {{$character->last_name}}</h3>
+			<h3 class="text-center">Apodo: {{$character->epithet}}</h3>
+			</td>
+			</tr>
+			<tr>
+			<td>
+			</td>
+			<td>
+			<h3 class="text-center">Rasa: {{$character->race}}</h3>
+			<h3 class="text-center">Genero: {{$character->gender}}</h3>
+			<h3 class="text-center">Altura: {{$character->height}}</h3>
+			<h3 class="text-center">Edad: {{$character->age}} años</h3>
+			<h3 class="text-center">Armas: {{$character->weapons}}</h3>
+			<h3 class="text-center">Afiliacion: {{$character->affiliation}}</h3>
+			<h3 class="text-center">Descripcion: {{$character->description}}</h3>
+			</td>
+			</tr>
+			</table><br><br><br><br>
 			<a href="/delete/{{$character->id}}" class="btn btn-primary">
 			Delete</a>
 			<a href="/characters/{{$character->id}}/edit" class="btn btn-secondary">
