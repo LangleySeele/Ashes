6d41eb5e3924b1621aa84d02c70f6696352218cc
LangleySeele <57202687+LangleySeele@users.noreply.github.com>
2019-12-01 01:26:20 -0600

    Update edit.blade.php
    se agrega la vista de error para la validacion

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.app')
 @section('title','Characters Edit')
 @section('content')
+
+@include('error')
 {!!Form::model($character,['route'=>['characters.update',$character],'method'=>'PUT','files'=>true])!!}
 @include('form')
 {{Form::submit('Actualizar',['class'=>'btn btn-primary'])}}
