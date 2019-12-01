a62bedac44f18c794940ccdb1ad2dc19153cef21
LangleySeele <57202687+LangleySeele@users.noreply.github.com>
2019-12-01 01:16:39 -0600

    Update Character.php
    modificacion del modelo Character para la busqueda

--- app/Character.php
+++ app/Character.php
@@ -10,4 +10,12 @@ class Character extends Model
     //
     use Searchable;
     protected $fillable=['name','last_name','avatar','epithet','race','gender','height','age','weapons','affiliation','description'];
+    
+    public function scopeName($query, $name)
+    {
+        if (trim($name) != "")
+        {
+            $query->where(\DB::raw("CONCAT(name, ' ', epithet)"), "LIKE", "%$name%");
+        }
+    }
 }
